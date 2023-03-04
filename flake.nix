b60f0565b5451a9060840e776f40910c9726c383
Hallam Smith <me@hcssmith.com>
2023-03-04 13:39:12 +0000

    Initialize Repo

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My nixpkgs overlay";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.default
+        ];
+        config = { allowUnfree = true; };
+      };
+    in {
+      overlays.default =  (final: prev: rec {
+        vimPlugins = prev.vimPlugins // import ./vimPlugins {inherit prev;};
+      });
+      packages.x86_64-linux = pkgs;
+    };
+}
