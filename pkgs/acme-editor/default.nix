486da5ae7180e21cc23b3143343d8f808dba4ae4
Halla Smith <me@hcssmith.com>
2023-03-10 17:58:07 +0000

    Fix script

--- pkgs/acme-editor/default.nix
+++ pkgs/acme-editor/default.nix
@@ -1,7 +1,5 @@
 {pkgs, plan9port, ...}:
-pkgs.runCommand "acme-editor" {
-  buildInputs = [ pkgs.plan9port ];
-} ''
-	${pkgs.plan9port}/bin/9 acme -f /mnt/font/SourceCodePro-Regular/24a/font &
-'';
+pkgs.writeShellScriptBin "acme-editor" ''
+  exec ${pkgs.plan9port}/bin/9 acme -f /mnt/font/SourceCodePro-Regular/24a/font &
+''
 
