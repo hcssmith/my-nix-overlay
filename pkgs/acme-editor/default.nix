bc4f03838220f05d8f0ff23e67d3df08967b4076
Halla Smith <me@hcssmith.com>
2023-03-12 21:45:22 +0000

    added autoindent option to acme-editor

--- pkgs/acme-editor/default.nix
+++ pkgs/acme-editor/default.nix
@@ -1,5 +1,5 @@
 {pkgs, plan9port, ...}:
 pkgs.writeShellScriptBin "acme-editor" ''
-  exec ${pkgs.plan9port}/bin/9 acme -f /mnt/font/SourceCodePro-Regular/20a/font &
+  exec ${pkgs.plan9port}/bin/9 acme -a -f /mnt/font/SourceCodePro-Regular/20a/font &
 ''
 
