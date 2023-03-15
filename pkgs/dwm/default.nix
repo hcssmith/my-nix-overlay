ed6797cfea05069ecfc5d783c100c62814d323e2
Halla Smith <me@hcssmith.com>
2023-03-15 08:21:07 +0000

    Bumped dwm version

--- pkgs/dwm/default.nix
+++ pkgs/dwm/default.nix
@@ -3,7 +3,7 @@ dwm.overrideAttrs (oldAttrs: rec{
   src = fetchFromGitHub {
     owner = "hcssmith";
     repo = "dwm";
-    rev = "511e2b3d8808c4bec61baba11ff8af1e17928625";
-    sha256 = "bBvZ4Mw6UkIkfPC724C18f9mQe8518kHSxCcNV5+/Tw=";
+    rev = "a62bc049d4cf001ae345c94133e83a32528d9f65";
+    sha256 = "uihqOZQJWpExDDmJe23Nd0SlzkC7KShz0sAiMlmGwp8=";
   };
 })
